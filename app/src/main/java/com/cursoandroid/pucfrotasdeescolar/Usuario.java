eca2ba3471754f81e094e283b55b8b1e98937475
nasciontem <lucas.nascimento@cbcloud.com.br>
2020-05-30 16:30:20 -0300

    [Ajuste] Cadastro

--- app/src/main/java/com/cursoandroid/pucfrotasdeescolar/Usuario.java
+++ app/src/main/java/com/cursoandroid/pucfrotasdeescolar/Usuario.java
@@ -1,17 +1,27 @@
 package com.cursoandroid.pucfrotasdeescolar;
 
+import android.util.Base64;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 public class Usuario {
-    private int id;
-    private String nome;
-    private String email;
-    private String senha;
-    private boolean status;
+    protected String id;
+    protected String nome;
+    protected String email;
+    protected String senha;
+    protected boolean status;
+    protected boolean usuarioJaCadastrado;
 
     public Usuario() {
         new Usuario("", "", "");
     }
 
-    public Usuario(String email, String senha){
+    public Usuario(String email, String senha) {
         this.email = email;
         this.senha = senha;
     }
@@ -22,20 +32,20 @@ public class Usuario {
         this.senha = senha;
     }
 
-    public Usuario(int id, String nome, String email, String senha) {
+    public Usuario(String id, String nome, String email, String senha) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
     }
 
-    public Usuario(String email, String senha, boolean status){
+    public Usuario(String email, String senha, boolean status) {
         this.email = email;
         this.senha = senha;
         this.status = status;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -43,27 +53,60 @@ public class Usuario {
         this.nome = nome;
     }
 
-    public void setEmail(String email) { this.email = email; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public void setSenha(String senha) {
         this.senha = senha;
     }
 
-    public void setStatus(boolean status) { this.status = status; }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
-    public int getId(){
+    public String getId() {
         return this.id;
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
 
-    public String getEmail() { return email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getSenha(){
+    public String getSenha() {
         return this.senha;
     }
 
-    public boolean getStatus() { return this.status; }
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public boolean create(final Usuario usuario, final DatabaseReference databaseReference) {
+        usuarioJaCadastrado = false;
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String idUsuario = Base64.encodeToString(usuario.getEmail().getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)", "");
+                usuario.setId(idUsuario);
+                usuarioJaCadastrado = dataSnapshot.hasChild(idUsuario);
+
+                if (usuarioJaCadastrado) {
+                    usuarioJaCadastrado = true;
+                } else {
+                    databaseReference.child(idUsuario).setValue(usuario);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return usuarioJaCadastrado;
+    }
 }
